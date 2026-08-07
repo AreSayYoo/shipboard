6ec05047cdc9874b3f3783c95eb0f44ff797a48e
Matt Arceo <32740884+AreSayYoo@users.noreply.github.com>
2026-08-06 21:45:56 -0400

    Add projects API routes and Project model (#3)
    * added projects route
    
    * refactor projects
    
    * added get project from id route & refactoring Project Class

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI(title="Shipboard")
+
+class Project(BaseModel):
+    id: int
+    name: str
+    status: str
+
+projects = [
+    Project(id= 1, name= "Learn Python", status= "ongoing"),
+]
 
 @app.get("/")
 def home():
@@ -11,3 +21,16 @@ def about():
     return {"name": "Shipboard",
             "description": "To-do list app created by Matt Arceo.",
             "purpose": "Designed to help keep my work in order and not miss anything important coming from all directions."}
+
+@app.get("/projects")
+def get_projects():
+    count = len(projects)
+    return {"projects": projects,
+            "count": count}
+
+@app.get("/projects/{project_id}")
+def get_project_id(project_id: int) -> Project:
+    for project in projects:
+        if project.id == project_id:
+            return project
+    raise HTTPException(status_code=404, detail="Project not found")

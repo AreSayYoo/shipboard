f22b3d8d792547a80922ee69ea90478c1f09826b
Matt Arceo <32740884+AreSayYoo@users.noreply.github.com>
2026-08-09 23:00:02 -0400

    add task creation test

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -10,3 +10,18 @@ def test_get_tasks():
     data = response.json() 
     assert "tasks" in data
     assert "count" in data
+
+def test_add_task():
+    response = client.post(
+        "/projects/1/tasks",
+        json={
+            "title": "Learn Syntax"
+        },
+    )
+    assert response.status_code == 200
+
+    data = response.json()
+    assert "id" in data
+    assert data["project_id"] == 1
+    assert data["title"] == "Learn Syntax"
+    assert data["completed"] is False

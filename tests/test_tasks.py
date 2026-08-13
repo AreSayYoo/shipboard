51ec7035bb5b256d4bf36fae97b5ee7987c92b72
Matt Arceo <32740884+AreSayYoo@users.noreply.github.com>
2026-08-12 21:01:16 -0400

    added complete task test

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -25,3 +25,19 @@ def test_add_task():
     assert data["project_id"] == 1
     assert data["title"] == "Learn Syntax"
     assert data["completed"] is False
+
+def test_complete_task():
+    create_response = client.post(
+        "/projects/1/tasks",
+        json={"title": "Complete me!!"},
+    )
+
+    created_task = create_response.json()
+    task_id = created_task["id"]
+
+    response = client.patch(f"/tasks/{task_id}/complete")
+    assert response.status_code == 200
+    data = response.json()
+
+    assert data["id"] == task_id
+    assert data["completed"] is True

2a968fdc70d282426e51a1ce37cfcbe9d608485d
Matt Arceo <32740884+AreSayYoo@users.noreply.github.com>
2026-08-16 15:36:00 -0400

    isolated test with fresh in-memory data

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+from app.data import projects, tasks
+from app.models import Project, Task
+
+@pytest.fixture(autouse=True)
+def reset_data():
+    print("resetting test data")
+    projects.clear()
+    tasks.clear()
+
+    projects.append(Project(
+        id=1, 
+        name="Learn Python",
+        status="ongoing"
+    ))
+
+    tasks.append(Task(
+        id=1,
+        project_id=1,
+        title="Download Python",
+        completed=False
+    ))
